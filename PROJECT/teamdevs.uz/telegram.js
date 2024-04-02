499dc4f1d479bef090e5fbdc8270e37388b8e89d
atwebuz <abdusattorov2004@mail.ru>
2024-04-02 11:43:18 +0500

    send form was fixed

---
+++ PROJECT/teamdevs.uz/telegram.js
@@ -0,0 +1,39 @@
+var telegram_bot_id = "7031497239:AAF0EinTIcdyQf-czB-LyqmnnzbZxuFQqqA";
+var chat_id = 5676930441;
+
+function sendTelegram(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    var u_name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+
+    message = "Ismi: " + u_name + "\nEmail: " + email + "\nTelefon: " + phone + "\nIzoh: " + message;
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+        // Optionally, you can show a success message or perform other actions after successful submission
+    });
+
+    // Clear form fields after submission
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("message").value = "";
+}
